173dbeca5764730cc93f4533dd51394b745dd9d6
김상우 <codribble@gimsang-uui-MacBookPro.local>
2026-04-28 18:20:08 +0900

    실습1] Next.js 에 스타일링 적용

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -15,17 +15,19 @@ export default async function Home() {
 
   return (
     <div className="min-h-screen p-8">
-      <h1 className="text-2xl mb-4">The Cat API</h1>
+      <h1 className="mb-4 font-bold text-2xl text-secondary text-center">
+        The Cat API
+      </h1>
 
       <main>
-        <div className="grid grid-cols-3 gap-1">
+        <div className="grid grid-cols-4 gap-4">
           {cats.map((cat) => (
-            <div key={cat.id} className="border">
+            <div key={cat.id} className="overflow-hidden border rounded-[10px]">
               <figure className="h-48">
                 <img
                   src={cat.url}
                   alt={cat.breeds[0]?.name || "고양이"}
-                  className="w-full h-full"
+                  className="w-full h-full object-cover"
                 />
               </figure>
               <div className="p-2">
