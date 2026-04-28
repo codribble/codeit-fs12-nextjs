13b5652875137f0a43b5bb443e3c2811781050be
김상우 <codribble@gimsang-uui-MacBookPro.local>
2026-04-28 18:55:42 +0900

    실습2] 정적 라우팅 (Static Routing)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,7 @@
 "use client";
 
+import Link from "next/link";
+
 export default function Header() {
   return (
     <header className="w-full bg-white shadow-md">
@@ -10,8 +12,8 @@ export default function Header() {
 
         <nav>
           <ul className="flex space-x-6">
-            <li>Home</li>
-            <li>Breeds</li>
+            <Link href={"/"}>Home</Link>
+            <Link href={"/breeds"}>Breeds</Link>
           </ul>
         </nav>
       </div>
