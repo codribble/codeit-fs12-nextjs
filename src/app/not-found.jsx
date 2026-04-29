657196109758d26bef18b44623eac6d0144a514e
김상우 <codribble@gimsang-uui-MacBookPro.local>
2026-04-29 18:30:08 +0900

    실습9] loading, error, not-found 처리 - not found

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import Link from "next/link";
 
 const notFound = () => {
-  return <div>404 - 페이지를 찾을 수 없습니다.</div>;
+  return (
+    <div>
+      <h2>404 - 페이지를 찾을 수 없습니다</h2>
+      <p>
+        <Link href="/">홈으로 이동</Link>
+      </p>
+    </div>
+  );
 };
 
 export default notFound;
