c6f0fa45a5191d98f715b8c8d83fb3e3a11235f4
김상우 <codribble@gimsang-uui-MacBookPro.local>
2026-04-29 18:09:29 +0900

    실습8] ISR, SSR 각각 적용해보기 - Home SSR

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,8 @@ import CatCardList from "@/components/ui/CatCardList";
 import Image from "next/image";
 import staticCat from "../../public/sampleCat.jpg";
 
-export const revalidate = 5; // 5초로 설정
+// export const revalidate = 5; // 5초로 설정
+export const dynamic = "force-dynamic";
 
 export default async function Home() {
   console.log("Home");
